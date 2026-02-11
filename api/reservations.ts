31bbfe279dae7b1924048f8752e5bbd4f1a765ee
Jae Voy Seloterio <sjaevoy@gmail.com>
2026-02-11 22:16:02 +0800

    Mobile pages and ui added

---
+++ api/reservations.ts
@@ -0,0 +1,31 @@
+import Constants from "expo-constants";
+
+
+export async function FetchReservations() {
+    const extras = Constants.expoConfig.extra ?? {};
+    const LIBRARY_API_URL = extras.LIBRARY_API_URL as string;
+    const LIBRARY_API_KEY = extras.LIBRARY_API_KEY as string;
+    const LIBRARY_ORIGIN = extras.LIBRARY_ORIGIN as string;
+
+    try {
+        const res = await fetch(`${LIBRARY_API_URL}/reservations/`, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'x-api-key': `${LIBRARY_API_KEY}`,
+                'Origin': `${LIBRARY_ORIGIN}`,
+                'Authorization': '2|qViPMAVbLC2WhnQWJ3EB6hPGVcQy4AK8t8PYL4uU2ea0a470'
+            }
+        });
+
+        const response = await res.json();
+        
+        if(!res.ok) {
+            throw new Error(response.message);
+        }
+
+        return response;
+    }catch(error) {
+        throw error;
+    }
+}
--- app/(tabs)/dashboard.tsx
+++ app/(tabs)/dashboard.tsx
@@ -13,6 +13,7 @@ export default function BooksView() {
     const [loading, setLoading] = useState(false);
     const [bookCount, setBookCount] = useState(0)
     const [borrowedBooks, setBorrowedBooks] = useState(0)
+    const [bookCopies, setBookCopies] = useState(0);
 
     useEffect(() => {
         handleFetchBooks();
@@ -24,7 +25,8 @@ export default function BooksView() {
 
             if(!response.error) {
                 setBookCount(response.book_count);
-                setBorrowedBooks(response.book_borrowed);
+                setBookCopies(response.copy_count);
+                setBorrowedBooks(response.borrowed_count);
             }
         }catch(error: any) {
             Alert.alert('Error', error.message);
@@ -51,24 +53,31 @@ export default function BooksView() {
                         <Ionicons name={'person-circle-outline'} size={28} color={'#fff'} />
                     </View>
                     <View style={{ paddingTop: 30, paddingHorizontal: 20, gap: 20 }}>
-                        <View style={{ width: '100%', height: 100, padding: 20, borderRadius: 10, shadowColor: "#000", position: 'relative' }}>
+                        <View style={{ flexDirection: 'row', alignItems: 'flex-start', justifyContent: 'space-between', width: '100%', height: 100, padding: 20, borderRadius: 10, shadowColor: "#000", position: 'relative' }}>
                             <LinearGradient
-                                colors={['#3498db', 'rgba(52, 152, 219, 0.88)',  'rgb(82, 179, 243)', 'rgba(241, 196, 15, 0.38)' ]}
-                                start={{ x: 0, y: 1 }}
-                                end={{ x: 1, y: 0 }}
+                                colors={['#3498db', 'rgba(52, 152, 219, 0.88)',  'rgb(82, 179, 243)',  'rgb(148, 205, 243)' ]}
+                                start={{ x: 0, y: 0 }}
+                                end={{ x: 0, y: 1 }}
                                 style={{ height: 100, position: 'absolute', width: width - 40 , bottom: 0, zIndex: -1, borderRadius: 10 }}
                             />
-                            <Text style={{ color: '#fff' }}>Books</Text>
-                            <View style={{ marginTop: 5, flexDirection: 'row', alignItems: 'center', gap: 5 }}>
-                                <MaterialCommunityIcons name={'bookshelf'} size={35} color={'#fff'} />
-                                <Text style={{ fontWeight: '900', fontSize: 28, color: '#fff' }}>{bookCount}</Text>
+                            <View>
+                                <Text style={{ color: '#fff' }}>Books</Text>
+                                <View style={{ marginTop: 5, flexDirection: 'row', alignItems: 'center', gap: 5 }}>
+                                    <MaterialCommunityIcons name={'bookshelf'} size={35} color={'#fff'} />
+                                    <Text style={{ fontWeight: '900', fontSize: 28, color: '#fff' }}>{bookCount}</Text>
+                                </View>
+                            </View>
+                            <View style={{ flexDirection: 'column', alignItems: 'flex-end' }}>
+                                <Text style={{ color: '#fff' }}>Book Copies</Text>
+                                <Text style={{ fontWeight: '900', fontSize: 20, color: '#fff' }}>{bookCopies}</Text>
                             </View>
                         </View>
+
                         <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'flex-end', width: '100%', height: 100, padding: 20, borderRadius: 10, shadowColor: "#000", position: 'relative' }}>
                             <LinearGradient
                                 colors={['#3498db', 'rgba(52, 152, 219, 0.88)',  'rgb(82, 179, 243)', 'rgb(148, 205, 243)' ]}
                                 start={{ x: 0, y: 0 }}
-                                end={{ x: 1, y: 0 }}
+                                end={{ x: 0, y: 1 }}
                                 style={{ height: 100, position: 'absolute', width: width - 40 , bottom: 0, zIndex: -1, borderRadius: 10 }}
                             />
                             <View>
@@ -78,7 +87,7 @@ export default function BooksView() {
                                     <Text style={{ fontWeight: '900', fontSize: 28, color: '#fff' }}>{borrowedBooks}</Text>
                                 </View>
                             </View>
-                            <TouchableOpacity style={{ flexDirection: 'row', alignItems: 'center', gap: 3 }}>
+                            <TouchableOpacity onPress={() => router.push('/borrowing')} style={{ flexDirection: 'row', alignItems: 'center', gap: 3 }}>
                                 <Ionicons name={'open-outline'} size={20} color={'#fff'} />
                                 <Text style={{ color: '#fff', fontSize: 16 }}>View</Text>
                             </TouchableOpacity>
@@ -87,9 +96,9 @@ export default function BooksView() {
                     <TouchableOpacity onPress={() => router.push('/borrow')} style={{ padding: 20, backgroundColor: '#3498db', borderRadius: 50, position: 'absolute', right: 20, bottom: 50 }}>
                         <MaterialCommunityIcons name={'qrcode-scan'} size={25} color={'#fff'} />
                     </TouchableOpacity>
-                    <TouchableOpacity onPress={() => handleLogout()} style={{ padding: 20, backgroundColor: '#3498db', borderRadius: 50, position: 'absolute', right: 20, bottom: 10 }}>
+                    {/* <TouchableOpacity onPress={() => handleLogout()} style={{ padding: 20, backgroundColor: '#3498db', borderRadius: 50, position: 'absolute', right: 20, bottom: 10 }}>
                         <Text>Logout</Text>
-                    </TouchableOpacity>
+                    </TouchableOpacity> */}
                 </>
             )}
         </View>
