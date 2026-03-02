e059044f05c588ab8b0d291449b038766fd7d9ad
lez133 <lezorogan10@gmail.com>
2026-03-02 16:41:08 +0800

    return scanner UI

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,6 @@
 import { BorrowedBookProvider } from "@/context/borrow";
 import { Stack } from "expo-router";
-import * as WebBrowser from 'expo-web-browser';
+import * as WebBrowser from "expo-web-browser";
 import { StatusBar } from "react-native";
 
 WebBrowser.maybeCompleteAuthSession();
@@ -8,16 +8,27 @@ WebBrowser.maybeCompleteAuthSession();
 export default function RootLayout() {
   return (
     <BorrowedBookProvider>
-      <StatusBar barStyle={'light-content'} backgroundColor={'transparent'} translucent />
+      <StatusBar
+        barStyle={"light-content"}
+        backgroundColor={"transparent"}
+        translucent
+      />
       <Stack>
-
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="borrow" options={{ headerShown: false }} />
         <Stack.Screen name="borrowing" options={{ headerShown: false }} />
+        <Stack.Screen name="returnbook" options={{ headerShown: false }} />
         <Stack.Screen name="borrowInfo" options={{ headerShown: false }} />
-        <Stack.Screen name="search" options={{ headerShown: false, animation: 'slide_from_right', animationDuration: 100 }} />
+        <Stack.Screen
+          name="search"
+          options={{
+            headerShown: false,
+            animation: "slide_from_right",
+            animationDuration: 100,
+          }}
+        />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       </Stack>
     </BorrowedBookProvider>
-  )
+  );
 }
